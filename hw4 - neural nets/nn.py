478e2a09530eec17a38be36a6183182881b7e79c
SebastijanT <79153493+strojanski@users.noreply.github.com>
2025-04-18 19:19:33 +0200

    Add regression

--- hw4 - neural nets/nn.py
+++ hw4 - neural nets/nn.py
@@ -3,89 +3,90 @@ import csv
 
 class Layer:
     def __init__(self, input_dim, output_dim):
-        self.input_dim = input_dim  # Input dimension = # of features (from prev layer)
-        self.output_dim = output_dim # Output dimension = # of neurons
-        self.weights = np.random.randn(input_dim, output_dim) * np.sqrt(1. / (input_dim)) * 2   # Xavier
-        self.biases = np.zeros((1, output_dim))
+        self.input_dim = input_dim # of features (from prev layer)
+        self.output_dim = output_dim  # of neurons
+        self.weights = np.random.randn(input_dim, output_dim) * np.sqrt(1. / (input_dim)) # Xavier
+        self.biases = np.ones((1, output_dim))
 
     def forward(self, X):
         self.X = X
         self.output = np.dot(X, self.weights) + self.biases
         return self.output
     
-    def backward(self, dZ):
-        self.dweights = np.dot(self.X.T, dZ)
-        self.dbiases = np.sum(dZ, axis=0, keepdims=True)
-        self.dX = np.dot(dZ, self.weights.T)
+    def backward(self, d_inputs):
+        self.dweights = np.dot(self.X.T, d_inputs)
+        self.dbiases = np.sum(d_inputs, axis=0, keepdims=True)
+        self.dX = np.dot(d_inputs, self.weights.T)
         return self.dX
     
 class Softmax:
-    """Softmax activation function, used in the last layer - outputs probabilities"""
-    def forward(self, Z):
-        self.Z = Z
+    """Softmax activation function, used in the last layer - outputs probabilities
+        Grad combined with loss in Softmax_CrossEntropyLoss
+    """
+    def forward(self, inputs):
+        self.inputs = inputs 
         
-        exp_Z = np.exp(Z - np.max(Z, axis=1, keepdims=True))
-        probs = exp_Z / np.sum(exp_Z, axis=1, keepdims=True)
+        exp_inputs = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probs = exp_inputs / np.sum(exp_inputs, axis=1, keepdims=True)
         
         self.probs = probs
         return probs
     
-    # def backward(self, dZ):
-    #     # return dZ
-    #     self.dinp = np.zeros_like(dZ)
-        
-    #     for i, (prob, dz) in enumerate(zip(self.probs, dZ)):
-    #         prob = prob.reshape(-1, 1) 
-            
-    #         # Jacobian
-    #         jacobian = np.diagflat(prob) - np.dot(prob, prob.T)
-    #         self.dinp[i] = np.dot(jacobian, dz)
-    #     return self.dinp
 
 class Sigmoid_Activation:
     """Sigmoid activation function, used in between hidden layers"""
     def __init__(self):
-        self.Z = None
+        self.inputs = None
         self.output = None
         self.dinput = None
         
-    def forward(self, Z):
-        self.Z = Z
-        self.output = 1 / (1 + np.exp(-1 * Z))
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-1 * inputs))
         return self.output
     
-    def backward(self, dZ):
-        self.dinput = dZ * self.output * (1 - self.output)
+    def backward(self, d_inputs):
+        self.dinput = d_inputs * self.output * (1 - self.output)
         return self.dinput
+    
+class ReLU_Activation:
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+        return self.output
+    
+    def backward(self, d_inputs):
+        self.dinput = d_inputs.copy()
+        self.dinput[self.inputs <= 0] = 0
+        return self.dinput
+    
+class LeakyReLU_Activation:
+    def forward(self, x):
+        self.input = x
+        return np.where(x > 0, x, 0.01 * x)
+
+    def backward(self, d_out):
+        self.d_out = d_out * np.where(self.input > 0, 1, 0.01) 
+        return self.d_out
     
 class CategoricalCrossEntropyLoss:
     """Categorical Cross-Entropy loss function, as the loss function (global)"""
     def forward(self, y_pred, y_true):
-        """y_pred = softmax output, y_true = labels, need to one-hot encode"""
+        """y_pred = softmax output, y_true = labels, need to one-hot encoded (done in this funciton)"""
         n_samples = len(y_true)
         
         y_pred = np.clip(y_pred, 1e-8, 1 - 1e-8)  # Avoid log(0)
         
         confidences = y_pred[range(n_samples), y_true]
         
-        # Onehot encoded
+        # Onehot encode
         if len(y_true.shape) == 2:
             confidences = np.sum(y_pred * y_true, axis=1)
             
         neg_log_likelihood = -np.log(confidences)
         return np.mean(neg_log_likelihood)
     
-    # def backward(self, dy, y_true):
-    #     n_samples = len(dy)
-    #     labels = len(dy[0])
-        
-    #     # Onehot
-    #     if len(y_true.shape) == 1:
-    #         y_true = np.eye(labels)[y_true]
-        
-    #     self.dinput = -y_true / dy
-    #     self.dinput /= n_samples    # Normalize gradient
-    #     return self.dinput   
 
 class Softmax_CrossEntropyLoss:
     def __init__(self):
@@ -108,24 +109,39 @@ class Softmax_CrossEntropyLoss:
         self.dinputs /= n_samples
         
         return self.dinputs
+    
+class Linear_Activation:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = inputs
+        return self.output
         
-
+    def backward(self, d_inputs):
+        self.dinputs = d_inputs
+        return self.dinputs
+        
+        
+class MeanSquaredErrorLoss:
+    def forward(self, y_pred, y_true):
+        return np.mean((y_pred - y_true) ** 2)
+    
+    def backward(self, y_pred, y_true):
+        return 2 * (y_pred - y_true) / y_pred.shape[0]
+    
 class SGD_Optimizer:
     def __init__(self, lr=0.01):
         self.lr = lr
         
     def update(self, layer):
+        np.clip(layer.dweights, -1, 1, out=layer.dweights)
+        np.clip(layer.dbiases, -1, 1, out=layer.dbiases)
+
         layer.weights -= self.lr * layer.dweights
         layer.biases -= self.lr * layer.dbiases
-        
+        # print(np.mean(layer.weights), np.mean(layer.biases))
+        # print(np.mean(layer.dweights), np.mean(layer.dbiases))
         return layer
 
-class ANNRegression:
-    # implement me too, please
-    pass
-
-
-# data reading
 
 def read_tab(fn, adict):
     content = list(csv.reader(open(fn, "rt"), delimiter="\t"))
@@ -155,7 +171,7 @@ class ANNClassification:
         self.softmax = Softmax()
         self.loss = CategoricalCrossEntropyLoss()
         self.softmax_cce = Softmax_CrossEntropyLoss()
-        self.optimizer = SGD_Optimizer(lr=1)
+        self.optimizer = SGD_Optimizer(lr=.001)
         self.layers = []
         self.n_iter = n_iter
         self.lambda_ = lambda_
@@ -166,7 +182,7 @@ class ANNClassification:
         
         for i in range(self.n_iter):
             # Decay
-            self.lr = 1 * 0.99**i
+            self.optimizer.lr = 1 * 0.99**i
             
             logits = self.forward_pass(X)
             probs, loss = self.softmax_cce.forward(logits, y)
@@ -183,7 +199,7 @@ class ANNClassification:
                     print(f"Loss: {loss:.4f}")
                     
             
-            self.backward_pass(X, y)
+            self.backward_pass(y)
             
         print(f"Accuracy: {acc:.4f}")
         print(f"Loss: {loss:.4f}")
@@ -210,7 +226,7 @@ class ANNClassification:
 
         return logits
     
-    def backward_pass(self, X, y):
+    def backward_pass(self, y):
         # Softmax + Loss 
         dL = self.softmax_cce.backward(y)
         
@@ -232,26 +248,104 @@ class ANNClassification:
         # predictions = np.argmax(probs, axis=1)
         # return np.eye(len(set(predictions)))[predictions]    
 
-from sklearn.model_selection import train_test_split
+
+class ANNRegression(ANNClassification):
+    def __init__(self, units, lambda_=0, n_iter=100000, verbose=False):
+        super().__init__(units, lambda_, n_iter, verbose)
+        self.loss = MeanSquaredErrorLoss()
+        self.optimizer = SGD_Optimizer(lr=1)
+        
+    def fit(self, X, y):
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
+            
+        self.build(X, y)
+        
+        for i in range(self.n_iter):
+            # Update optimizer's learning rate with decay
+            if i % 1000 == 0:
+                self.optimizer.lr = np.maximum(1 * 0.999**i, 1e-7)
+                print(self.optimizer.lr)
+            
+            logits = self.forward_pass(X)
+            loss = self.loss.forward(logits, y)
+            
+            if i % 100 == 0: 
+                if np.mean(loss) < 0.01:
+                    print(f"Converged after {i} iterations")
+                    break
+                if self.verbose:
+                    print(f"Loss: {np.mean(loss):.4f}")
+                    
+            self.backward_pass(y, logits)
+            # for layer in self.layers:
+            #     print("Weights std:", np.std(layer.weights))
+            #     print("Biases std:", np.std(layer.biases))
+            #     print("dWeights std:", np.std(layer.dweights))
+            #     print("dBiases std:", np.std(layer.dbiases))
+
+        return self
+            
+    def build(self, X, y):
+        self.units = [X.shape[1]] + self.units + [1]
+        self.layers = []
+        for i in range(len(self.units) - 1):
+            self.layers.append(Layer(self.units[i], self.units[i + 1]))
+            
+        self.activations = [LeakyReLU_Activation() for _ in range(len(self.units) - 2)]
+            
+    def forward_pass(self, X):
+        inp = X
+        for i, layer in enumerate(self.layers[:-1]):
+            inp = layer.forward(inp)
+            inp = self.activations[i].forward(inp)
+        logits = self.layers[-1].forward(inp)
+        
+        return logits
+            
+            
+    def backward_pass(self, y, logits):
+        d_loss = self.loss.backward(logits, y)
+        d = d_loss
+        
+        d = self.layers[-1].backward(d)
+
+        for act, layer in zip(reversed(self.activations), reversed(self.layers[:-1])):
+            d = act.backward(d)
+            d = layer.backward(d)
+        
+        for layer in self.layers:
+            self.optimizer.update(layer)     
+        
+    
+
 if __name__ == "__main__":
 
-    fitter = ANNClassification(units=[10, 10], lambda_=0., verbose=True)
+    # fitter = ANNClassification(units=[3], lambda_=0., verbose=True)
+    fitter = ANNRegression(units=[10,10,10], lambda_=0., verbose=True)
     X, y = squares()
-    X, y = doughnut()
+
+    # Create X with values from 1 to 10 with a step of 0.01
+    X = np.arange(1, 10, 0.01).reshape(-1, 1)
+
+    # Create y as the sine of each value in X
+    y = np.cos(X).reshape(-1, 1)
+
+    
+    fitter.fit(X, y)
+    # X, y = doughnut()
+    
+    np.save("fitted_cos.npy", fitter.layers[-1].output)
     
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    print(X_train.shape, y_train.shape)
-    
-    
-    m = fitter.fit(X_train, y_train)
-    print()
-    pred = m.predict(X_test)
-    
-    pred = np.argmax(pred, axis=1)
-    print("Accuracy on test set:", np.mean(pred == y_test))
-    
+    # m = fitter.fit(X_train, y_train)
     exit()
+    # pred = m.predict(X_test)
+    
+    # pred = np.argmax(pred, axis=1)
+    # print("Accuracy on test set:", np.mean(pred == y_test))
+    
+    # exit()
     
     
     X = np.array([
