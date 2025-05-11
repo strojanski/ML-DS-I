9a49110703e1d885a8a262325cab1f123441894b
SebastijanT <79153493+strojanski@users.noreply.github.com>
2025-05-11 12:40:35 +0200

    TESTS WORK

--- hw5 - kernels/hw_kernels copy.py
+++ hw5 - kernels/hw_kernels copy.py
@@ -4,8 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 
-def polynomial_kernel(x1, x2, M=2):
-    return (np.dot(x1, x2.T) + 1) ** M
+def standardize(X):
+    X_mean = np.mean(X)
+    X_std = np.std(X)
+    
+    return (X - X_mean) / X_std, X_mean, X_std
+
+def unstandardize(X, X_mean, X_std):
+    return X * X_std + X_mean
+
+def polynomial_kernel(x1, x2, coef=1, M=2):
+    return (np.dot(x1, x2.T) + coef) ** M
 
 def rbf_kernel(X1, X2, gamma=1.0):
     X1_sq = np.sum(X1**2, axis=1)[:, np.newaxis]
@@ -13,22 +22,23 @@ def rbf_kernel(X1, X2, gamma=1.0):
     dist_sq = X1_sq + X2_sq - 2 * np.dot(X1, X2.T)
     return np.exp(-gamma * dist_sq)
 
-def gram_mtx(X, kernel, M=11):
-    return kernel(X, X, M=M) if kernel == polynomial_kernel else kernel(X, X, gamma=1)
+def gram_mtx(X, kernel, M=11, gamma=1):
+    return kernel(X, X, M=M) if kernel == polynomial_kernel else kernel(X, X, gamma=gamma)
 
 def ridge_regression(X, y, lmbda, kernel="polynomial", M=11):
     kernel = polynomial_kernel if kernel == "polynomial" else rbf_kernel
     K = gram_mtx(X, kernel, M)
     n = K.shape[0]
+    
 
     alpha = np.linalg.solve(K + lmbda * np.eye(n), y)
+    
     return alpha, kernel
 
    
 def svr(X, y, lmbda, kernel="polynomial", eps=1e-3, M=11, gamma=0.4):
     # 0.5 [alpha, alpha*].T @ P @ [alpha, alpha*] + q.T @ [alpha, alpha*]
     # K = similarity(X, X) in kernel space
-    #
     
     l = X.shape[0]
     C = 1.0 / lmbda
@@ -36,8 +46,8 @@ def svr(X, y, lmbda, kernel="polynomial", eps=1e-3, M=11, gamma=0.4):
     perm = np.array([i//2 + (i % 2) * l for i in range(2 * l)])
 
     K = polynomial_kernel if kernel=="polynomial" else rbf_kernel
-    K = K(X, X, M) if kernel == "polynomial" else K(X, X, gamma=gamma)
-    K = K.astype(np.double)
+    K = gram_mtx(X, K, M, gamma)
+    K += 1e-8 * np.eye(K.shape[0])
 
     # Quadratic term - 0.5 * [a a*].T @ P @ [a a*] = aKa - 2aKa* + a*Ka* => P = [K -K; -K K]
     P = np.vstack([
@@ -50,8 +60,8 @@ def svr(X, y, lmbda, kernel="polynomial", eps=1e-3, M=11, gamma=0.4):
                    eps * np.ones((l,1)) + y])   # eps + y
 
     # Inequality ontraints - between 0 and C (Gx <= h)
-    G = np.vstack([np.eye(2*l),                 # lower bound
-                   -np.eye(2*l)])               # upper bound
+    G = np.vstack([-np.eye(2*l),                 # lower bound
+                   np.eye(2*l)])               # upper bound
     
     h = np.vstack([np.zeros((2*l,1)),           # l = 0
                    C*np.ones((2*l,1))])         # u = C
@@ -73,28 +83,35 @@ def svr(X, y, lmbda, kernel="polynomial", eps=1e-3, M=11, gamma=0.4):
     
     z = np.array(sol['x']).flatten()
     
-    alpha     = z[::2]
+    alpha     =  z[::2]
     alpha_star = z[1::2]
 
     weights = alpha - alpha_star
-    thresh = 1e-7
-    support_vectors = np.where((alpha>thresh) & (alpha<C-thresh) &
-                  (alpha_star>thresh) & (alpha_star<C-thresh))[0]   # Nonzero
-    print("Support Vectors:", support_vectors)
-    print("Alpha:", alpha)
-    print("Alpha*:", alpha_star)
 
-    if support_vectors.size==0:
-        sv = np.where(np.abs(weights)>1e-8)[0]  # If nothing found
+    def compute_bias(K, y, alpha, alpha_star, C, thresh=1e-4):
+        weights = alpha - alpha_star
+        sv = np.where(((alpha > thresh) & (alpha < C - thresh)) |
+                    ((alpha_star > thresh) & (alpha_star < C - thresh)))[0]
+        
+        if len(sv) == 0:
+            sv = np.where(np.abs(weights) > 1e-4)[0]  # fallback
+        
+        b_vals = []
+        for i in sv:
+            sum_k = np.sum(weights * K[:, i])
+            b_vals.append(y[i] - sum_k)
+        return float(np.mean(b_vals)), sv
 
-    b_val = np.mean([y[i] - np.dot(weights, K[:,i]) for i in sv]) # bias
+    b_val, support_vectors = compute_bias(K, y, alpha, alpha_star, C, thresh=1e-4)
+    print(len(support_vectors), "support vectors")
 
     def predict(X_new, M=11):
         K_new = polynomial_kernel if kernel=="polynomial" else rbf_kernel
-        K_new = K_new(X_new, X, M) if kernel == "polynomial" else K_new(X_new, X, gamma=1)  
-        return K_new @ weights + b_val
+        K_new = K_new(X_new, X[support_vectors], M) if kernel == "polynomial" else K_new(X_new, X[support_vectors], gamma=gamma)  
+        
+        return K_new @ weights[support_vectors] + b_val
 
-    return alpha, alpha_star, b_val, predict
+    return alpha, alpha_star, b_val, predict, support_vectors
 
 
 def krr_train_mse(X, y, kernel_type="rbf", lambdas=[1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1], gammas=[0.1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Ms=[2, 3, 5, 7, 9, 11]):
@@ -142,10 +159,9 @@ def krr_train_mse(X, y, kernel_type="rbf", lambdas=[1e-7, 1e-6, 1e-5, 1e-4, 1e-3
 if __name__ == "__main__":
     data = np.loadtxt("sine.csv", delimiter=",", dtype=float, skiprows=1)
     X = data[:, 0]
-    
-    X = (X - np.mean(X)) / np.std(X)
-    
     y = data[:, 1]
+    
+
     X = X.reshape(-1, 1)
     y = y.reshape(-1, 1)
     
@@ -153,36 +169,63 @@ if __name__ == "__main__":
     print(X.shape, y.shape)
     # y = y.reshape(-1, 1)
     
-    kernel_ = "rbf"
-    # best_params, best_score = krr_train_mse(X, y, kernel_type=kernel)
+    kernel_ = "polynomial"
+    # best_params, best_score = krr_train_mse(X, y, kernel_type=kernel_)
     # print(best_params, best_score)
 
     
     lmbda = 1e-7 if kernel_ == "polynomial" else 1e-4
-    M = 11
+    M = 7
+    
+    X, X_mean, X_std = standardize(X)
+    y, y_mean, y_std = standardize(y)
+    
     
     alpha, kernel = ridge_regression(X, y.reshape(-1, 1), lmbda, kernel=kernel_, M=M)
+    support_vectors = np.where((alpha > 1e-6) & (alpha < 1 / lmbda - 1e-6))[0]
     
     K = kernel(X, X, M=M) if kernel_ == "polynomial" else kernel(X, X, gamma=1)
     y_plot = K @ alpha
     
+    X = unstandardize(X, X_mean, X_std)
+    y_plot = unstandardize(y_plot, y_mean, y_std)
+    y = unstandardize(y, y_mean, y_std)
+    
+
+    # plt.scatter(X, y, label="Data")
+    # plt.scatter(X, y_plot, label=f"Kernel Ridge Regression ({kernel_})", color="red")
+    # plt.scatter(X[support_vectors], y[support_vectors], color="green", label="Support Vectors")
+    # plt.legend()
+    # plt.show()
+
+
+
+    data = np.loadtxt("sine.csv", delimiter=",", dtype=float, skiprows=1)
+    X = data[:, 0]
+    y = data[:, 1]
+    
+
+    X = X.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+
+
+    lmbda_rbf = 1e-3
+    lmbda = 1e-7
+    M = 5
+    X, X_mean, X_std = standardize(X)
+    y, y_mean, y_std = standardize(y)
+    
+    alpha, alpha_star, b, predict, sv = svr(X, y, lmbda=lmbda, kernel=kernel_, eps=.001, M=M, gamma=1)
+
+    
+    y_plot = predict(X, M=M)
+
+    X = unstandardize(X, X_mean, X_std)
+    y_plot = unstandardize(y_plot, y_mean, y_std)
+    y = unstandardize(y, y_mean, y_std)
 
     plt.scatter(X, y, label="Data")
-    plt.scatter(X, y_plot, label=f"Kernel Ridge Regression ({kernel_})", color="red")
-    plt.legend()
-    plt.show()
-
-    # lmbda = 0.001
-
-
-    alpha, alpha_star, b, predict = svr(X, y, lmbda=lmbda, kernel=kernel_, eps=1e-5, M=M, gamma=1)
-    
-    y_pred = predict(X)
-    
-    X_plot = np.linspace(X.min(), X.max(), 300).reshape(-1, 1)
-    y_plot = predict(X_plot, M=M)
-
-    plt.scatter(X, y, label="Data")
-    plt.scatter(X_plot, y_plot, color="red", label=f"SVR Prediction ({kernel_})")
+    plt.scatter(X, y_plot, color="red", label=f"SVR Prediction ({kernel_})")
+    plt.scatter(X[sv], y[sv], color="green", label="Support Vectors")
     plt.legend()
     plt.show()
---
+++ hw5 - kernels/hw_kernels.py
@@ -0,0 +1,199 @@
+from cvxopt import matrix, solvers 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+
+def standardize(X):
+    X_mean = np.mean(X, axis=0)
+    X_std = np.std(X, axis=0) + 1e-8  
+    
+    return (X - X_mean) / X_std, X_mean, X_std
+
+def unstandardize(X, X_mean, X_std):
+    return X * X_std + X_mean
+
+
+class Polynomial:
+    def __init__(self, M=2, coef0=1):
+        self.degree = M
+        self.coef0 = coef0
+
+    def __call__(self, X1, X2):
+        X1 = np.atleast_2d(X1)
+        X2 = np.atleast_2d(X2)
+        result = (np.dot(X1, X2.T) + self.coef0) ** self.degree
+        
+        if X1.shape[0] == 1 and X2.shape[0] == 1:
+            return result[0, 0]  
+        if X1.shape[0] == 1 or X2.shape[0] == 1:
+            return result.flatten()  
+        return result  
+
+class RBF:
+    def __init__(self, sigma=1.0):
+        self.sigma = sigma
+
+    def __call__(self, X1, X2):
+        X1 = np.atleast_2d(X1)
+        X2 = np.atleast_2d(X2)
+        
+        gamma = 1.0 / (2 * self.sigma ** 2)
+        
+        X1_sq = np.sum(X1**2, axis=1)[:, np.newaxis]
+        X2_sq = np.sum(X2**2, axis=1)[np.newaxis, :]
+        
+        dist_sq = X1_sq + X2_sq - 2 * np.dot(X1, X2.T)
+        result = np.exp(-gamma * dist_sq)
+        
+        
+        if X1.shape[0] == 1 and X2.shape[0] == 1:
+            return result[0, 0]
+        if X1.shape[0] == 1 or X2.shape[0] == 1:
+            return result.flatten() 
+        return result 
+
+
+    
+class KernelizedRidgeRegression:
+    def __init__(self, kernel, lambda_, threshold=1e-6):
+        self.kernel = kernel
+        self.lambda_ = lambda_
+        self.threshold = threshold
+        self.C = 1.0 / lambda_
+        
+    
+    def fit(self, X, y):
+        X, self.X_mean, self.X_std = standardize(X)
+        y, self.y_mean, self.y_std = standardize(y)
+        
+        K = self.kernel(X, X)
+        self.X = X
+        
+        n = K.shape[0]
+        self.alpha = np.linalg.solve(K + self.lambda_ * np.eye(n), y)
+        self.support_vectors = np.where((self.alpha > self.threshold) & (self.alpha < self.C - self.threshold))[0]
+
+        return self
+
+    def predict(self, X_new):
+        X_new = (X_new - self.X_mean) / self.X_std
+        K_new = self.kernel(X_new, self.X)
+        y_pred = K_new @ self.alpha
+        return y_pred * self.y_std + self.y_mean
+    
+    
+class SVR:
+    def __init__(self, kernel, lambda_, epsilon, threshold=1e-5):
+        self.kernel = kernel
+        self.lambda_ = lambda_
+        self.epsilon = epsilon
+        self.threshold = threshold
+        self.C = 1.0 / lambda_
+        
+    def get_alpha(self):
+        return np.column_stack([self.alpha, self.alpha_star])
+        
+    def get_b(self):
+        return self.bias
+    
+    def fit(self, X, y):
+        X, self.X_mean, self.X_std = standardize(X)
+        # y, self.y_mean, self.y_std = standardize(y)
+        # print(y, self.y_mean, self.y_std)
+        
+        self.X = X
+        K = self.kernel(X, X)
+        n = K.shape[0]
+        perm = np.array([i//2 + (i % 2) * n for i in range(2 * n)])
+        
+        # Quadratic term - 0.5 * [a a*].T @ P @ [a a*] = aKa - 2aKa* + a*Ka* => P = [K -K; -K K]
+        P = np.block([
+            [K, -K],
+            [-K, K]
+        ])
+
+        # Linear term = bias
+        q = np.concatenate([self.epsilon - y,       # eps - y
+                            self.epsilon + y]       # eps + y
+                           ).astype(np.double)  
+
+        # Inequality ontraints - between 0 and C (Gx <= h)
+        G = np.vstack([-np.eye(2*n), np.eye(2*n)])               # upper bound
+        
+        h = np.hstack([np.zeros(2*n),           # l = 0
+                       np.ones(2*n) * self.C])         # u = C
+        
+        # # Equality constraints - sum(alpha - alpha*) = 0 (Ax = b)
+        A = np.hstack([np.ones((1,n)), -np.ones((1,n))])  # alpha - alpha*
+        
+        b = np.zeros((1))   # = 0
+        
+        # Permute so that we get [a1, a1*, a2, a2*, ...]
+        P = P[perm][:, perm]
+        q = q[perm]
+        G = G[perm, :]
+        h = h[perm]
+        A = A[:, perm]
+        
+        self.sol = solvers.qp(matrix(P), matrix(q),
+                              matrix(G), matrix(h),
+                              matrix(A), matrix(b))
+        
+        x = np.array(self.sol["x"]).flatten()
+        self.bias = self.sol["y"][0]
+        
+        self.alpha     =  x[::2]
+        self.alpha_star = x[1::2]
+        
+        self.weights = self.alpha - self.alpha_star
+
+        self.support_vectors = self.compute_sv()
+        print(self.alpha, self.alpha_star)
+
+        return self#, matrix(P), matrix(q), matrix(G), matrix(h), matrix(A), matrix(b)
+
+    def compute_sv(self):
+        sv = np.where(((self.alpha > self.threshold) & (self.alpha < self.C - self.threshold)) |
+                    ((self.alpha_star > self.threshold) & (self.alpha_star < self.C - self.threshold)))[0]
+
+        return sv
+
+    def predict(self, X_new):
+        X_new = (X_new - self.X_mean) / self.X_std
+        
+        K_new = self.kernel(X_new, self.X)
+        y_pred = K_new @ (self.alpha - self.alpha_star) + float(self.bias)
+        print(self.bias)
+        
+        return y_pred # * self.y_std + self.y_mean 
+
+
+
+
+if __name__ == "__main__":
+    data = np.loadtxt("sine.csv", delimiter=",", dtype=float, skiprows=1)
+    X = data[:, 0]
+    y = data[:, 1]
+    
+
+    X = X.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+    
+    
+    print(X.shape, y.shape)
+    # y = y.reshape(-1, 1)
+    
+    kernel_ = "polynomial"
+    # best_params, best_score = krr_train_mse(X, y, kernel_type=kernel_)
+    # print(best_params, best_score)
+
+
+    # plt.scatter(X, y, label="Data")
+    # plt.scatter(X, y_plot, label=f"Kernel Ridge Regression ({kernel_})", color="red")
+    # plt.scatter(X[support_vectors], y[support_vectors], color="green", label="Support Vectors")
+    # plt.legend()
+    # plt.show()
+
+
+
